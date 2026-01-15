17a0be9352c1e1f89710d03b4695769177d6b848
hafizi1997 <116898680+hafizi1997@users.noreply.github.com>
2026-01-16 03:26:10 +0800

    add package

---
+++ app/layout.tsx
@@ -0,0 +1,18 @@
+import "../public/assets/css/zeena.css"
+import 'swiper/css'
+// import "swiper/css/navigation"
+import "swiper/css/pagination"
+import 'swiper/css/free-mode';
+import { openSans, poppins } from '@/lib/font'
+export const metadata = {
+    title: 'Zeena',
+    description: 'Next Js template'
+}
+
+export default function RootLayout({ children }) {
+    return (
+        <html lang="en" className={`${openSans.variable} ${poppins.variable}`}>
+            <body>{children}</body>
+        </html>
+    )
+}
