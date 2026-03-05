83c819bec0db441bce68afc2cfeb4d62ac4efbca
hafizi1997 <116898680+hafizi1997@users.noreply.github.com>
2026-03-05 09:54:15 +0800

    fix contact page

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -3,12 +3,12 @@ import Link from "next/link";
 export default function Home() {
   return (
     <>
-      <Layout headerStyle={2} footerStyle={1} breadcrumbTitle="Contact">
+      <Layout headerStyle={3} footerStyle={1} breadcrumbTitle="Contact">
         {/*Google Map Start*/}
         <section className="google-map-one">
           <div className="container">
             <iframe
-              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d4562.753041141002!2d-118.80123790098536!3d34.152323469614075!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x80e82469c2162619%3A0xba03efb7998eef6d!2sCostco+Wholesale!5e0!3m2!1sbn!2sbd!4v1562518641290!5m2!1sbn!2sbd"
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3984.016972429654!2d101.62124867581336!3d3.090140053510808!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x31cc4be0d0219e99%3A0xecc3cbbaa08ca8ca!2sCube%20Self%20Storage%20(Petaling%20Jaya)!5e0!3m2!1sen!2ssg!4v1772613969972!5m2!1sen!2ssg"
               className="google-map__one"
               allowFullScreen=""
             />
@@ -31,8 +31,9 @@ export default function Home() {
                       <div className="content">
                         <h3>Address</h3>
                         <p>
-                          16 vastu arcade, 5rd Floor
-                          <br /> palace road, London.
+                          Cube Self Storage Suite 2.1, Seksyen 51a, 46100,
+                          Petaling Jaya,
+                          <br /> Selangor, Malaysia.
                         </p>
                       </div>
                     </li>
@@ -43,10 +44,10 @@ export default function Home() {
                       <div className="content">
                         <h3>Phone</h3>
                         <p>
-                          <Link href="tel:882562562584">88 256 256 2584</Link>
+                          <Link href="tel:60193623141">+60 19-3623141</Link>
                         </p>
                         <p>
-                          <Link href="tel:882562562584">88 256 256 2584</Link>
+                          <Link href="tel:60198176949">+60 19-8176949</Link>
                         </p>
                       </div>
                     </li>
@@ -63,7 +64,7 @@ export default function Home() {
                         </p>
                         <p>
                           <Link href="mailto:info-team@amkatechnologies.com">
-                            info-team@amkatechnologies.com
+                            contact@amkatech.com
                           </Link>
                         </p>
                       </div>
